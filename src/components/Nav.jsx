e528cf04b38a067f95e0be858668c3cd42622665
Jimoh Habeeblahi Adesola <122438648+CyberWarBaby@users.noreply.github.com>
2026-01-14 23:28:02 +0100

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 
+
 export default function Nav(){
   const { token, userEmail, logout } = useAuth()
   const nav = useNavigate()
